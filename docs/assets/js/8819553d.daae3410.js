5940f1e2b3ac1de475dfacd10d89765132753712
Viktor Lidholt <viktorlidholt@gmail.com>
2024-01-15 15:33:57 +0100

    1.2.0 release.

---
+++ docs/assets/js/8819553d.daae3410.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkserverpod_docs=self.webpackChunkserverpod_docs||[]).push([[1584],{83769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
