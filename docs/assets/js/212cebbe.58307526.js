357a8472ecd0cce861acbfa80a6a7d9a98f5b18c
Alexander Sandor <alexander@serverpod.dev>
2024-01-22 11:49:11 +0100

    fix: Deploy fixes to 1.2 documentation

---
+++ docs/assets/js/212cebbe.58307526.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkserverpod_docs=self.webpackChunkserverpod_docs||[]).push([[4778],{83769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
